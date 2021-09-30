542163ab5f0c24b3b869d23f47718473c7537f96
formulaB <91692191+Trombone2024@users.noreply.github.com>
2021-09-30 17:38:50 -0600

    Calculator.py
    Python-made Calculator, calculating basic stuff such as multiplication, addition, division, and subtraction. more options coming soon! Stay tuned :)

--- Calculator.py
+++ Calculator.py
@@ -16,16 +16,19 @@ def divide(x,y):
 
 def power(x,y):
     return x^y
-    
+
+print('_________________________')   
 print('What do you want to do?')
 print('Addition')
 print('subtraction')
 print('multiplication')
 print('division')
 print('power')
+print('_________________________')  
 
 while True:
-    choice=input('Choose either + (addition) or - (subtraction) or * (multiplication) or / (division) or ^ (power)')
+    choice=input('Choose either + (addition) or - (subtraction) or * (multiplication) or / (division) or ^ (power)|')
+    print('_________________________')
     
     if choice in('+','-','*','/','^'):
         num1 = float(input('Type first number: '))
@@ -34,6 +37,47 @@ while True:
         if choice == '+':
             print(num1, '+', num2, '=', add(num1, num2))
             
+        elif choice == '-':
+            print(num1, '-', num2, '=', subtract(num1, num2))
+            
+        elif choice == '*':
+            print(num1, '*', num2, '=', multiply(num1, num2))
+            
+        elif choice == '/':
+            print(num1, '/', num2, '=', divide(num1, num2))
+            
+        elif choice == '^':
+            print(num1, '^', num2, '=', add(num1, num2))
+            
+        next_calculation = input('Want to ask another quetion? (yes/no)')
+        if next_calculation == 'no':
+            next_calculation = input('Do you want to determine a factorial then? (ok/no)')
+            if next_calculation == 'ok':
+                num = int(input("Enter a number: "))
+
+
+                factorial = 1
+
+                if num < 0:
+                    print("Sorry, factorial does not exist for negative numbers")
+                elif num == 0:
+                    print("The factorial of 0 is 1")
+                else:
+                    for i in range(1,num + 1):
+                        factorial = factorial*i
+                print("The factorial of",num,"is",factorial)
+                
+            if next_calculation == 'no':
+                break
+        
+        
+        
+else:
+    print('invalid')
+
+
+
+
         elif choice == '-':
             print(num1, '-', num2, '=', subtract(num1, num2))
             
