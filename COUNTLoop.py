aa1f68fb5fa3460f0dcd075d36f435d245126d4f
formulaB <91692191+Trombone2024@users.noreply.github.com>
2021-10-21 09:07:51 -0600

    COUNTKeyLoop.py
    This loop uses the "count" function.

---
+++ COUNTLoop.py
@@ -0,0 +1,13 @@
+##==================================================================
+from time import sleep
+##==================================================================
+count = 0 
+while count <= 99.99999999999999:
+    print ('type ig,')
+    sleep(0.01)
+    print('add whatever you want it to loop')
+    sleep(0.01)
+    print('you dont have to include the sleep key')
+    sleep(0.1)
+    print('but i like it so i used it.')
+##=================================================================
